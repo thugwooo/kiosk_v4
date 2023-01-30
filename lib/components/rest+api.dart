d0da6dd97e6eed86cd8d312f2ab1e8711dd8f07f
LeeSeokJune <indians1720@gmail.com>
2023-01-30 22:59:59 +0900

    prototype

--- lib/components/rest+api.dart
+++ lib/components/rest+api.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 
-var basic_url = 'http://52.79.233.120:8000/';
-// var basic_url = 'https://louishome.shop//';
+// var basic_url = 'http://52.79.233.120:8000/';
+var basic_url = 'https://louishome.shop//';
 var dio = Dio();
 
 Future post_data({url, data}) async {
